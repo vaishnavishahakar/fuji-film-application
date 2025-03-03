2bad7f1a089b73409da9c8ce7eaf227a948a9826
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-03-03 13:43:38 +0530

    created data in database through API

--- server/index.js
+++ server/index.js
@@ -7,29 +7,75 @@ dotenv.config();
 
 // Custom imports
 import { getHome, getHealth, getNotFound } from './controllers/other.js';
+import Film from "./models/Film.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const connectDB = async()=>{
+const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     if(conn){
         console.log("Connected to MongoDB");
     }
-}
+};
 
 app.get("/", getHome);
 app.get("/health", getHealth);
 
+// Film
+app.post("/films", async(req, res) => {
+const {
+    title,
+    shortDescription,
+    director,
+    poster,
+    releaseYear,
+    category,
+    language,
+    rating,
+    writer,
+    review
+} = req.body;
 
+const newFilm = new Film({
+    title,
+    shortDescription,
+    director,
+    poster,
+    releaseYear,
+    category,
+    language,
+    rating,
+    writer,
+    review
+});
 
+const savedFilm = await newFilm.save();
+
+    return res.status(201).json({
+        success: true,
+        message: "Film created",
+        data: savedFilm,
+    });
+});
+
+app.get("/films", async(req, res) => {
+    const films = await Film.find();
+    return res.status(200).json({
+        success: true,
+        data: films,
+        message: "All films fetched",
+    });
+});
 
 app.get("*", getNotFound);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+connectDB().then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
--- server/models/Film.js
+++ server/models/Film.js
@@ -2,15 +2,18 @@ import {model, Schema} from 'mongoose';
 
 const filmSchema = new Schema({
     title: String,
-    director: String,
+    shortDescription: String,
+    director: Array,
     poster: String,
     releaseYear: Number,
     category: String,
     language: String,
     rating: Number,
-    cast: String,
-    producer: String,
-});
+    writer: Array,
+    review: String,
+},
+{timestamps: true}
+);
 
 const Film = model('Film', filmSchema);
 
