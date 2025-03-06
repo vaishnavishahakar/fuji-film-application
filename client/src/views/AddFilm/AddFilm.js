a144b60057733f7370647672b0444617ead2e66a
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-03-06 12:58:33 +0530

    declare useState variables

--- client/src/views/AddFilm/AddFilm.js
+++ client/src/views/AddFilm/AddFilm.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddFilm.css';
 
 function AddFilm () {
+    const [film, setFilm] = useState({
+        title:"",
+        shortDescription:"",
+        director:"",
+        poster:"",
+        releaseYear:"",
+        category:"",
+        language:"",
+        writer:"",
+        rating:"",
+        review: "",
+    });
+
   return (
     <div>
-        <h1>Add Film</h1>
+        <h1 className='heading'>Add Film</h1>
+
+        <div className='add-film-form'>
+
+            <div className='input-container'>
+                <label htmlFor='title' className='input-label'>Title</label>
+                <input type='text' id='title' className='input-element' />
+            </div>
+
+        </div>
     </div>
   )
 }
